d3f72073f53c477fbe16916f63ba6e3c7b7c1a46
Troels Henriksen <athas@sigkill.dk>
2012-03-07 15:12:03 +0100

    Document the Parser typeclass.

--- Text/Parco.hs
+++ Text/Parco.hs
@@ -19,8 +19,18 @@ module Text.Parco
 
 import Control.Applicative
 
+-- | Some parser combinators need special operations that are not
+-- captured in any of the standard typeclasses (backtracking, for
+-- example).  For these, it is necessary to define an instance of this
+-- class.
 class Alternative p => Parser p where
+  -- | If @try p@ fails, no input should be consumed.  The default
+  -- method calls 'error'.
   try :: p a -> p a
   try = error "Cannot backtrack"
+  -- | If @expects p s@ fails, the error message should note that @s@
+  -- (a descriptive string) was expected, but has otherwise no
+  -- semantic effect on the parser.  The default method simply returns
+  -- @p@.
   expects :: p a -> String -> p a
   expects p _ = p
